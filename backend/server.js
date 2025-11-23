fcda04233209eb2778d4479880823508fd89c48d
SK Mousin <skkayes13@gmail.com>
2025-11-23 21:14:23 +0530

    update

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,9 @@ const dbconnect = require("./src/db/db");
 
 dbconnect();
 
-app.listen(3000, ()=> {
-    console.log("Server is running on port: 3000");
-})
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port: ${PORT}`);
+});
+
+module.exports = app;
--- backend/src/app.js
+++ backend/src/app.js
@@ -9,12 +9,11 @@ const userRoutes = require("../src/routes/user.routes");
 
 const app = express();
 app.use(cors({
-    origin: ['http://localhost:5173', 'https://food-reels-t369.vercel.app'],
+    origin: ['http://localhost:5173', 'https://food-reels-t369.vercel.app', 'https://food-reels-one.vercel.app'],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization']
 }));
-
 
 app.use(cookieParser());
 app.use(express.json());
